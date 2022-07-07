61e8f69a39dc609317b799c553e3386deff43b0d
Miriam Efedhoma <83411128+Khessie@users.noreply.github.com>
2022-07-07 16:16:47 +0000

    third commit

--- src/assignment.js
+++ src/assignment.js
@@ -21,20 +21,20 @@ function sumOfNumbers(arrayOfNumbers) {
 }
 assignment.sumOfNumbers = sumOfNumbers;
 
-// /**
-//  * Challenge - 2
-//  * 
-//  * Count all even numbers within an array of numbers.
-//  * If you did Challenge - 2, remove the comment in the line just after this function
-//  * 
-//  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
-//  * @returns number the count of even numbers
-//  */
-// function countEvenNumbers(arrayOfNumbers) {
+/**
+ * Challenge - 2
+ * 
+ * Count all even numbers within an array of numbers.
+ * If you did Challenge - 2, remove the comment in the line just after this function
+ * 
+ * @param {Array} arrayOfNumbers the array containing even or non-even numbers
+ * @returns number the count of even numbers
+ */
+function countEvenNumbers(arrayOfNumbers) {
 
-//     return arrayOfNumbers.length;
-// }
-// assignment.countEvenNumbers = countEvenNumbers;
+    return arrayOfNumbers.length;
+}
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3

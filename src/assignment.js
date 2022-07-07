5c8d3d542bfcb8367262a6b27e4829fd57742a50
Miriam Efedhoma <83411128+Khessie@users.noreply.github.com>
2022-07-07 15:55:06 +0000

    First Commit

--- src/assignment.js
+++ src/assignment.js
@@ -4,15 +4,15 @@ const assignment = {};
 // ========================
 
 
-/**
- * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
- */
+// /**
+//  * Challenge - 1
+//  * 
+//  * Calculate and return the sum of the numbers in an array.
+//  * If you did Challenge - 1, remove the comment in the line just after this function
+//  * 
+//  * @param {Array} arrayOfNumbers the array of numbers to sum
+//  * @returns number the sum of the numbers
+//  */
 function sumOfNumbers(arrayOfNumbers) {
     let sum = 0;
     for (let i = 0; i < arrayOfNumbers.length; i++)
@@ -21,20 +21,20 @@ function sumOfNumbers(arrayOfNumbers) {
 }
 assignment.sumOfNumbers = sumOfNumbers;
 
-/**
- * Challenge - 2
- * 
- * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
- */
+// /**
+//  * Challenge - 2
+//  * 
+//  * Count all even numbers within an array of numbers.
+//  * If you did Challenge - 2, remove the comment in the line just after this function
+//  * 
+//  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
+//  * @returns number the count of even numbers
+//  */
 function countEvenNumbers(arrayOfNumbers) {
 
     return arrayOfNumbers.length;
 }
-assignment.countEvenNumbers = countEvenNumbers;
+// assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
